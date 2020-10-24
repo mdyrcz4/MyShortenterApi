897a7a955b6eeb234bd6a25d28e5d5cac967c3d5
mdyrcz4 <mdyrcz4@gmail.com>
2020-10-24 22:14:48 +0200

    Update Startup.cs

--- MyShortenterApi/Startup.cs
+++ MyShortenterApi/Startup.cs
@@ -23,16 +23,7 @@ namespace MyShortenterApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                                .AllowAnyMethod()
-                                .AllowAnyHeader();
-                    });
-            });
+            services.AddCors();
             
             services.AddControllers();
             services.AddBehaviours();
@@ -76,11 +67,16 @@ namespace MyShortenterApi
                 });
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseCors();
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
             
             app.UseEndpoints(endpoints =>
             {
