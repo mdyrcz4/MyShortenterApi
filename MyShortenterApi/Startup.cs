064b9c5d22f8593f8b828c15bce82fd43a6ea2a9
mdyrcz4 <mdyrcz4@gmail.com>
2020-10-24 21:36:59 +0200

    Adding default cors policy

--- MyShortenterApi/Startup.cs
+++ MyShortenterApi/Startup.cs
@@ -23,6 +23,17 @@ namespace MyShortenterApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader();
+                    });
+            });
+            
             services.AddControllers();
             services.AddBehaviours();
             services.AddServices();
@@ -69,6 +80,8 @@ namespace MyShortenterApi
 
             app.UseRouting();
 
+            app.UseCors();
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
